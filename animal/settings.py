9a47d976122f0060e1ed5c2a4e7a4534011a3371
Aditya Kumar <ironaditya12345@gmail.com>
2025-03-14 15:47:43 +0530

    Refactor settings to use environment variables for sensitive data and update .gitignore to include virtual environment files

--- animal/settings.py
+++ animal/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from decouple import config
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&ydm2wfh15zep(gm1d^ux_$vi04#p00)2ef(_ip5$htzm=mdd4'
+SECRET_KEY = config('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
